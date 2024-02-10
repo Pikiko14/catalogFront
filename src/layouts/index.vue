15e67632827e539908448eedda3d11459e5c4a20
olasanmi <maalhequi95@gmail.com>
2024-02-10 09:38:15 -0500

    fix router login and header

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-primary" v-if="route.path == '/'">
+    <q-header class="bg-primary" v-if="route.path == '/hide'">
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" />
 
